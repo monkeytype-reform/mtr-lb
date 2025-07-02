360329a97091a9e5ae0c67e350683d0096f783bf
Xyntho <krishnab0406@gmail.com>
2025-07-02 17:23:14 +0530

    improve pb resolution logic (return null if pb not found)

--- modules/personalBests.js
+++ modules/personalBests.js
@@ -1,7 +1,14 @@
 export function getBestScores(pbData) {
-    let resolvedPBData = {};
+    let resolvedPBData = {
+        time: {
+            '15': null,
+            '60': null
+        },
+        words: {
+            '10': null
+        },
+    };
     for(let mode in pbData) {
-        resolvedPBData[mode] = {};
         for (let mode2 in pbData[mode]) {
             const bestScoreInMode = (pbData[mode][mode2] ?? []).sort((a, b) => b.wpm - a.wpm)[0];
             resolvedPBData[mode][mode2] = bestScoreInMode;
